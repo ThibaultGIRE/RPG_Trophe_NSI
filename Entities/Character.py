f604f1d2028105ceec1690b1898dbb6fb1445c7e
ThibaultGIRE <thibault.girardreydet@gmail.com>
2025-12-03 14:49:57 +0100

    Modification de character

--- Entities/Character.py
+++ Entities/Character.py
@@ -1,14 +1,25 @@
 from Entities.Attack import Attack
 
 class Personnage:
-    def __init__(self, nom, points_de_vie, niveau, pv, pv_max, attack, defense, speed, position, attacks):
-        '''Initialisation du personnage. Notes : position est un tuple (x,y) et attacks est une liste. Différencier attacks de attack'''
-        self.nom = nom
-        self.points_de_vie = points_de_vie
-        self.niveau = niveau
+    def __init__(self, name,level, hp, hp_max, attack, defense, speed, position, attacks):
+        '''Initialize a Charadter instance.
+
+        Args:
+            name (str): character's name
+            level (int): current level
+            hp (int): current health points
+            hp_max (int): maximum health point
+            attack (int): stat for attack power
+            defense (int): stat for defense power
+            speed (int): stat for speed
+            position (tuple): character's positon as (x, y)
+            attacks (list): list of Attack objects
+        '''
+        self.name = name
+        self.level = level
         self.xp = 0
-        self.pv = pv
-        self.pv_max = pv_max
+        self.hp = hp
+        self.hp_max = hp_max
         self.attack = attack
         self.defense = defense
         self.speed = speed
@@ -16,25 +27,30 @@ class Personnage:
         self.attacks = attacks #liste 
 
     def move(self, dx, dy):
-        '''Déplace le personnage de dx en x et dy en y'''
+        """move the character
+
+        Args:
+            dx (int): number of step in x direction
+            dy (int): number of step in y direction
+        """
         x, y = self.position
         self.position = (x + dx, y + dy)
 
     def take_damage(self, damage):
         '''Réduit les points de vie du personnage en fonction des dégâts reçus'''
-        self.points_de_vie -= damage
-        if self.points_de_vie < 0:
-            self.points_de_vie = 0
+        self.hp -= damage
+        if self.hp < 0:
+            self.hp = 0
 
     def heal(self, amount):
         '''Soigne le personnage d'une certaine quantité'''
-        self.points_de_vie += amount
-        if self.points_de_vie > self.pv_max:
-            self.points_de_vie = self.pv_max
+        self.hp += amount
+        if self.hp > self.hp_max:
+            self.hp = self.hp_max
     
     def is_alive(self):
         '''Retourne True si le personnage est en vie, False sinon'''
-        return self.points_de_vie > 0
+        return self.hp > 0
     
     def do_attack(self, target, attack_obj):
         '''Faire une attaque'''
