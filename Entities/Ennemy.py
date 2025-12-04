98cf773b6781525003f089a17da6552882217d65
ThibaultGIRE <thibault.girardreydet@gmail.com>
2025-12-04 09:42:10 +0100

    ajout de la méthode choose_target

--- Entities/Ennemy.py
+++ Entities/Ennemy.py
@@ -5,7 +5,26 @@ class Enemy(Personnage):
         super().__init__(nom, points_de_vie, niveau, pv, pv_max, attack, defense, speed, position, attacks)
 
     def decide_action(self, map, player):
+        # let the class AiControler decide the action
         pass
 
     def choose_target(self, players):
-        pass
+        """Choose the player with the lowest hp
+
+        Args:
+            players (list): list of player characters
+
+        Returns:
+            bool: None if there's no player alive
+            obj: player xith the lowest hp otherwise
+        """
+        alive_players = []
+
+        for player in players:
+            if player.is_alive():
+                alive_players.append(player)
+
+        if not alive_players:
+            return None
+        
+        return min(alive_players, key = lambda player: player.hp)
