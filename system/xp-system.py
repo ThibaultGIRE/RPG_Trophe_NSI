293de2619f013959d49cd70e7b5354a7e2521d31
ThibaultGIRE <thibault.girardreydet@gmail.com>
2025-12-05 10:57:33 +0100

    modification de level up

--- system/xp-system.py
+++ system/xp-system.py
@@ -1,7 +1,7 @@
 from Entities.Character import Personnage
-from random import randint as r
+from random import randint
 
-class Xp_system:
+class XpSystem:
 
     @staticmethod
     def required_xp(level):
@@ -9,23 +9,38 @@ class Xp_system:
     
     @staticmethod
     def gain_xp(xp, character):
-        requi_xp = Xp_system.required_xp(character.level)
+        requi_xp = XpSystem.required_xp(character.level)
         character.xp += xp
         if character.xp >= requi_xp:
             character.xp -= requi_xp
-            Xp_system.level_up(character)
+            XpSystem.level_up(character)
 
 
     @staticmethod
     def level_up(character):
         character.level += 1
-        if r(0, 100) < 60:
+        XpSystem.apply_stat_growth(character)
+
+    @staticmethod
+    def apply_stat_growth(character):
+
+        growth_rates = {
+            'hp' : 60,
+            'attack' : 45,
+            'defense' : 40,
+            'speed' : 50
+        }       
+
+        if randint(0, 100) < growth_rates['hp']:
             character.hp += 1
             character.hp_max += 1
-        if r(0, 100) < 45:
+
+        if randint(0, 100) < growth_rates['attack']:
             character.attack += 1
-        if r(0,100) < 40:
+
+        if randint(0,100) < growth_rates['defense']:
             character.defense += 1
-        if r(0, 100) < 50:
+
+        if randint(0, 100) < growth_rates['speed']:
             character.speed += 1
         
