f4675bcf030052bbcbc0dcb2e1448ae4b019432b
ThibaultGIRE <thibault.girardreydet@gmail.com>
2025-12-10 22:11:13 +0100

    modification de enemy_spawner

--- system/enemy_spawner.py
+++ system/enemy_spawner.py
@@ -11,6 +11,14 @@ class Ennemy_Spawner:
         }
         
     def spawn_wave(self, player_level):
+        """Spawn a wave with a bunch of enemies
+
+        Args:
+            player_level (int): player's level
+
+        Returns:
+            list: list of enemies spawned
+        """
         from random import randint
 
         num_enemies = randint(self.spawn_rules["min_enemies"], self.spawn_rules["max_enemies"])
@@ -24,7 +32,16 @@ class Ennemy_Spawner:
         return enemies
     
     def spawn_enemies(self, enemy_type, level):
+        from random import randint
+        
         base_stat_enemy = stat_de_base[enemy_type["type"]]
         enemy = Enemy(enemy_type["name"], base_stat_enemy["level"], base_stat_enemy["hp"], base_stat_enemy["hp_max"], base_stat_enemy["attack"], base_stat_enemy["defense"], base_stat_enemy["speed"], (0,0), enemy_type["attaques"])
+
+        while True:
+            x = randint(0, self.map.width - 1)
+            y = randint(0, self.map.height - 1)
+            if self.map.place_character(enemy, (x, y)):
+                break
+
         return enemy
     
