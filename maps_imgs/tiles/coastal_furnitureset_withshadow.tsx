077ed6049fb3b5cda6a925d23d35bb534130f147
julie_30.12 <julieganier2009@gmail.com>
2025-12-11 10:05:13 +0100

    ajout de nouvelle carte + tuiles

---
+++ maps_imgs/tiles/coastal_furnitureset_withshadow.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="coastal_furnitureset_withshadow" tilewidth="32" tileheight="32" tilecount="168" columns="14">
+ <image source="../../Downloads/tilesets/coastal_furnitureset_withshadow.png" width="448" height="384"/>
+</tileset>
---
+++ maps_imgs/tiles/messy furniture set updated v2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="messy furniture set updated v2" tilewidth="32" tileheight="32" tilecount="156" columns="12">
+ <image source="../../Downloads/tilesets/messy furniture set updated v2.png" width="384" height="416"/>
+</tileset>
