cd2e10ee342b9d0bb6e54b1476a427a9d8f19121
ThibaultGIRE <thibault.girardreydet@gmail.com>
2026-01-30 12:03:15 +0100

    Add living room map and associated tilesets
    - Created a new tiled project for the living room map.
    - Added a session file for the living room map with various tile states and configurations.
    - Included multiple PNG images for furniture and door tiles.
    - Added tilesets for various furniture and door styles, ensuring correct dimensions and sources.
    - Removed outdated kitchen tileset.
    - Updated coastal furniture tilesets to reflect new dimensions and sources.
    - Added new tilesets for messy and midcentury modern furniture and door tiles.
    - Included additional assets for neon font and obstacles.

---
+++ maps_imgs/tiles/neon_font.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="neon_font" tilewidth="16" tileheight="16" tilecount="78" columns="13">
+ <image source="../neon_font.png" width="208" height="96"/>
+</tileset>
